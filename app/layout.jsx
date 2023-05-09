725c18fe818f7054598e63b9b58b424e197fcc88
drakeaifoce <drakeaifoce@gmail.com>
2023-05-09 21:08:11 +0600

    Init layout header

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,25 @@
 import "./tailwind.css";
+import Image from "next/image";
+import { Button } from "../components/Button";
 
 export default function Layout({ children }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <header>
+          <nav className="container mx-auto flex flex-row justify-between items-center px-4 pb-14 pt-10">
+            <Image src="/vcnsea.svg" alt="vcnsea icon" width={84} height={13} />
+            <div className="flex flex-row gap-4">
+              <Button variant="tertiary" type="button">
+                Войти
+              </Button>
+              <Button variant="primary" type="button">
+                Начать пользоваться
+              </Button>
+            </div>
+          </nav>
+        </header>
+        <main>{children}</main>
       </body>
     </html>
   );
