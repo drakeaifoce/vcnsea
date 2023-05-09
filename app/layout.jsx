b571904691b7c1412213dd1006673bdd18be5ed2
Azamat Zulpykhar <zlpkhr@icloud.com>
2023-05-09 16:09:47 +0600

    Initialized reshaped

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,15 @@
+import { Reshaped } from "reshaped";
+import "reshaped/themes/reshaped/theme.css";
 import './tailwind.css'
 
 export default function Layout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Reshaped>
+          {children}
+        </Reshaped>
+      </body>
     </html>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 export default function Index() {
   return (
-    <main className="text-lg font-bold">
+    <main className="bg-neutral-faded rounded-small p-x4 l:p-x6">
       Hello world
     </main>
   )
