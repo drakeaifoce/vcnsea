013ce41673c4ab5a3ebdefcb28e6d097a4351a7e
drakeaifoce <drakeaifoce@gmail.com>
2023-05-09 21:10:35 +0600

    Change styling

--- components/Input.jsx
+++ components/Input.jsx
@@ -10,7 +10,7 @@ export const Input = forwardRef(
         name={name}
         placeholder={placeholder}
         className={clsx(
-          "px-4 py-2 font-normal text-base text-sage-12 bg-sage-3 border border-sage-7 rounded-md  hover:bg-sage-4 hover:border-sage-8 focus:bg-sage-5 focus:border-sage-8 outline-none",
+          "px-4 py-2 font-normal text-base text-sage-12 bg-sage-3 rounded-md hover:bg-sage-4 focus:bg-sage-5 focus:text-black outline-none",
           className
         )}
         {...props}
