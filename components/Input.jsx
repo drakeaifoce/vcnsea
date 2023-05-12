cd19d1e67049e540ddc1cf2574fe7d0157c4607a
drakeaifoce <drakeaifoce@gmail.com>
2023-05-12 15:36:37 +0600

    Add optional label for component

--- components/Input.jsx
+++ components/Input.jsx
@@ -2,19 +2,42 @@ import clsx from "clsx";
 import { forwardRef } from "react";
 
 export const Input = forwardRef(
-  ({ className, variant, name, id, placeholder, ...props }, ref) => {
+  ({ className, variant, name, id, placeholder, label, ...props }, ref) => {
     return (
-      <input
-        ref={ref}
-        id={id}
-        name={name}
-        placeholder={placeholder}
-        className={clsx(
-          "rounded-md border-transparent bg-sage-3 px-4 py-2 text-base font-normal text-sage-12 outline-none hover:bg-sage-4 focus:border-transparent focus:bg-sage-5 focus:text-black focus:ring-0",
-          className,
+      <>
+        {label ? (
+          <div className="flex flex-col">
+            {label && (
+              <label className="text-sm font-medium" htmlFor={name}>
+                {label}
+              </label>
+            )}
+            <input
+              ref={ref}
+              id={id}
+              name={name}
+              placeholder={placeholder}
+              className={clsx(
+                "rounded-md border-transparent bg-sage-3 px-4 py-2 text-base font-normal text-sage-12 outline-none hover:bg-sage-4 focus:border-transparent focus:bg-sage-5 focus:text-black focus:ring-0",
+                className,
+              )}
+              {...props}
+            />
+          </div>
+        ) : (
+          <input
+            ref={ref}
+            id={id}
+            name={name}
+            placeholder={placeholder}
+            className={clsx(
+              "rounded-md border-transparent bg-sage-3 px-4 py-2 text-base font-normal text-sage-12 outline-none hover:bg-sage-4 focus:border-transparent focus:bg-sage-5 focus:text-black focus:ring-0",
+              className,
+            )}
+            {...props}
+          />
         )}
-        {...props}
-      />
+      </>
     );
   },
 );
