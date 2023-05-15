65c35558ac596ae3241c91bf239532111286932b
drakeaifoce <drakeaifoce@gmail.com>
2023-05-15 14:00:17 +0600

    Delete date formater due to not using

--- app/company/[id]/interviews/page.jsx
+++ app/company/[id]/interviews/page.jsx
@@ -1,21 +1,6 @@
 import Link from "next/link";
 import { prisma } from "../../../prisma";
 
-function dateFormater(date, separator) {
-  let day = date.getDate();
-  let month = date.getMonth() + 1;
-  const year = date.getFullYear();
-
-  if (day < 10) {
-    day = "0" + day;
-  }
-  if (month < 10) {
-    month = "0" + month;
-  }
-
-  return day + separator + month + separator + year;
-}
-
 export default async function Interviews({ params }) {
   const interviews = await prisma.appointment.findMany({
     where: {
