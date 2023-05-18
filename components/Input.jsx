2047a656ac733daf38e13c0a92a596eb61b32fb2
drakeaifoce <drakeaifoce@gmail.com>
2023-05-18 17:31:59 +0600

    Restyle input primitive

--- components/Input.jsx
+++ components/Input.jsx
@@ -2,45 +2,29 @@ import clsx from "clsx";
 import { forwardRef } from "react";
 
 export const Input = forwardRef(
-  ({ className, name, id, placeholder, label, ...props }, ref) => {
+  ({ className, name, id, placeholder, label, required, ...props }, ref) => {
     return (
-      <>
-        {label ? (
-          <div className="flex w-full flex-col">
-            {label && (
-              <label
-                className="text-sm font-normal text-sage-10"
-                htmlFor={name}
-              >
-                {label}
-              </label>
-            )}
-            <input
-              ref={ref}
-              id={id}
-              name={name}
-              placeholder={placeholder}
-              className={clsx(
-                "rounded-md border-transparent bg-sage-3 px-4 py-2 text-base font-normal text-sage-12 outline-none hover:bg-sage-4 focus:border-transparent focus:bg-sage-5 focus:text-black focus:ring-0",
-                className,
-              )}
-              {...props}
-            />
-          </div>
-        ) : (
-          <input
-            ref={ref}
-            id={id}
-            name={name}
-            placeholder={placeholder}
-            className={clsx(
-              "rounded-md border-transparent bg-sage-3 px-4 py-2 text-base font-normal text-sage-12 outline-none hover:bg-sage-4 focus:border-transparent focus:bg-sage-5 focus:text-black focus:ring-0",
-              className,
-            )}
-            {...props}
-          />
+      <div className="flex w-full flex-col">
+        {label && (
+          <label className="text-sm font-normal text-gray" htmlFor={name}>
+            {label}
+          </label>
         )}
-      </>
+        <input
+          ref={ref}
+          id={id}
+          name={name}
+          placeholder={placeholder}
+          required={required}
+          className={clsx(
+            `${
+              required && "border-red-primary"
+            }border border-black px-4 py-2 text-base font-normal text-black outline-none focus:text-black focus:ring-0 md:px-5 md:py-3 lg:px-6 lg:py-4`,
+            className,
+          )}
+          {...props}
+        />
+      </div>
     );
   },
 );
