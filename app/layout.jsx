ceb3d84aa331644eafd77b942be5664d6e15412a
drakeaifoce <drakeaifoce@gmail.com>
2023-05-18 14:54:43 +0600

    Remake layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,24 @@
 import "./tailwind.css";
+import { Jost } from "next/font/google";
 import { Header } from "../components/Header";
 import Providers from "../components/Providers";
-import { SideBar } from "../components/SideBar";
 
 export const metadata = {
   title: "vcnsea",
   description: "Web-application for students to find vacancies",
 };
 
+const jost = Jost({ subsets: ["latin"] });
+
 export default function Layout({ children }) {
   return (
     <html lang="en">
       <body>
         <Providers>
-          <div className="flex flex-row">
-            <SideBar />
-            <main className="flex flex-1 flex-col px-6">
-              <Header />
-              <>{children}</>
-            </main>
-          </div>
+          <Header />
+          <main className={`${jost.className} container mx-auto px-4 py-8`}>
+            {children}
+          </main>
         </Providers>
       </body>
     </html>
