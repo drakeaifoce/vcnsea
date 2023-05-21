93c1377eb9549b9d7552997147d7c3e9744b60d7
drakeaifoce <drakeaifoce@gmail.com>
2023-05-21 20:19:04 +0600

    Restlye user account

--- app/user/[id]/page.jsx
+++ app/user/[id]/page.jsx
@@ -26,10 +26,10 @@ export default async function UserAccount({ params }) {
 
   return (
     <>
-      <div className="flex flex-col gap-8">
-        <div className="flex flex-row items-center gap-8">
+      <div className=":sm:gap-6 flex flex-col gap-4 px-4 text-black md:gap-8 lg:gap-10">
+        <div className="flex flex-col items-center gap-8 md:flex-row">
           <Image
-            src={`https://ui-avatars.com/api/name=${
+            src={`https://ui-avatars.com/api/?background=BBB9BA&color=090909&name=${
               student.firstName + student.secondName
             }`}
             unoptimized
@@ -39,24 +39,25 @@ export default async function UserAccount({ params }) {
             alt={`${student.firstName + student.secondName}`}
           />
           <>
-            <h1 className="text-xl font-medium text-black">
+            <h1 className="text-lg font-bold sm:text-xl md:text-2xl lg:text-3xl">
               {student.firstName + " " + student.secondName}
             </h1>
-            <h2 className="text-sm font-normal text-sage-11">
-              {student.specialty
-                ? student.specialty
-                : ("" + ", " + student.city
-                ? student.city
-                : "")}{" "}
+            <h2 className="text-sm font-normal md:text-base lg:text-lg">
+              {student.specialty && student.city
+                ? student.specialty + ", " + student.city
+                : " "}
             </h2>
-            {student.webiste && (
+            {student.website && (
               <Link
                 href={student.website}
-                className="text-sm font-normal text-sage-9 hover:underline"
+                className="text-sm font-normal text-blue-8 hover:underline md:text-base lg:text-lg"
               >
                 {student.website}
               </Link>
             )}
+            <address className="text-sm font-normal not-italic md:text-base lg:text-lg">
+              {student.email ? student.email : " "}
+            </address>
           </>
           <Link href={`/user/${params.id}/edit`}>
             <Button variant="primary">Edit</Button>
@@ -64,21 +65,25 @@ export default async function UserAccount({ params }) {
         </div>
         <>
           {student.quote && (
-            <p className="rounded-xl border-2 border-sage-4 px-6 py-4 text-sm font-normal text-sage-12">
+            <p className="border bg-orange-primary px-6 py-4 text-sm font-normal md:text-base lg:text-lg">
               {student.quote}
             </p>
           )}
         </>
 
         <div className="flex flex-col gap-1">
-          <h3 className="text-sm font-medium text-black">About</h3>
-          <p className="ml-4 text-sm font-normal text-sage-11">
+          <h3 className="text-base font-medium text-black md:text-lg lg:text-xl">
+            About
+          </h3>
+          <p className="text-sm font-normal md:text-base lg:text-lg">
             {student.about}
           </p>
         </div>
         <div className="flex flex-col gap-6">
           <div className="flex w-full flex-row items-center gap-9">
-            <h3 className="text-sm font-medium text-black">Work experience</h3>
+            <h3 className="text-base font-medium text-black md:text-lg lg:text-xl">
+              Work experience
+            </h3>
             <Link
               href={`/user/${params.id}/add-work-experience`}
               className="text-sm "
@@ -89,28 +94,31 @@ export default async function UserAccount({ params }) {
           <div className="flex flex-col gap-9">
             {student.workExperiences.map((experience) => {
               return (
-                <div key={experience.id} className="flex flex-row gap-9">
-                  <h4 className="text-sm font-normal text-sage-9">
+                <div
+                  key={experience.id}
+                  className="grid grid-cols-2 md:grid-cols-3"
+                >
+                  <h4 className="col-span-1 text-sm font-normal md:text-base lg:text-lg">
                     {" "}
                     {experience.start_date + " -"}
                     {experience.end_date}
                   </h4>
                   <div className="flex flex-col gap-2">
                     <div className="flex flex-col">
-                      <h4 className="text-sm font-normal text-black">
+                      <h4 className="col-span-1 text-sm font-normal md:text-base lg:text-lg">
                         {experience.title}
                       </h4>
                       <Link
                         href={experience.company_link}
-                        className="text-sm font-normal text-sage-12 hover:underline"
+                        className="text-base hover:underline md:text-lg lg:text-xl"
                       >
                         {experience.company_name}
                       </Link>
-                      <address className="text-sm font-normal not-italic text-sage-9">
+                      <address className="not-italic">
                         {experience.city}
                       </address>
                     </div>
-                    <p className="text-sm font-normal text-sage-11">
+                    <p className="text-sage-11 text-sm font-normal">
                       {experience.description}
                     </p>
                   </div>
