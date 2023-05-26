d03372f0c11d7740efcc38811186e838b6e37a90
drakeaifoce <drakeaifoce@gmail.com>
2023-05-26 19:42:44 +0600

    Add footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,9 +16,18 @@ export default function Layout({ children }) {
       <body>
         <Providers>
           <Header />
-          <main className={`${jost.className} container mx-auto px-4 py-8`}>
+          <main
+            className={`${jost.className} container mx-auto min-h-screen px-4 py-8`}
+          >
             {children}
           </main>
+          <footer className="bottom-0 bg-blue-9">
+            <section
+              className={`${jost.className} container mx-auto px-10 py-10 text-center text-xl font-extrabold text-white sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl`}
+            >
+              Oops, you reached the bottom of the page
+            </section>
+          </footer>
         </Providers>
       </body>
     </html>
